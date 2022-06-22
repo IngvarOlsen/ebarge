67b5e4e641becbcafcfcf56715ad5965a4db67b7
IngvarOlsen <ingvarolsen57@hotmail.com>
2022-06-23 00:26:31 +0200

    add example string calls

--- restApiCallExmaple.py
+++ restApiCallExmaple.py
@@ -4,11 +4,15 @@ import requests
 # Part of the school project
 
 api_url = "http://20.238.120.222:5000/reading_post"
-stringToSend = "testerStringData"
+stringToSend1 = "2,22,44,33,4.9"
+stringToSend2 = "3,29,49,59,4.9"
 jsonToSend = {"userId": 1, "title": "Wash car", "completed": True}
-responseData = requests.post(api_url + "_data", stringToSend)
-print(responseData.status_code)
+responseData1 = requests.post(api_url + "_data", stringToSend1)
+print(responseData1.status_code)
+responseData2 = requests.post(api_url + "_data", stringToSend2)
+print(responseData2.status_code)
 
-responseJson = requests.post(api_url + "_json", json=jsonToSend)
-print(responseJson.status_code)
+
+#responseJson = requests.post(api_url + "_json", json=jsonToSend)
+#print(responseJson.status_code)
 
